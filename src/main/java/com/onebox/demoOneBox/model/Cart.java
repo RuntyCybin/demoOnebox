14442a5077b3cc6e4f741970026611f29e347af4
m4st3 <runtycybin@gmail.com>
2023-03-01 18:08:54 +0100

    Model consists of Cart and Product classes

--- src/main/java/com/onebox/demoOneBox/model/Cart.java
+++ src/main/java/com/onebox/demoOneBox/model/Cart.java
@@ -1,26 +1,14 @@
 package com.onebox.demoOneBox.model;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-@Table(name = "cart")
 public class Cart {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+    private Integer id;
     private String cartname;
-
-    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Product> products;
 
 }
--- src/main/java/com/onebox/demoOneBox/model/Product.java
+++ src/main/java/com/onebox/demoOneBox/model/Product.java
@@ -1,6 +1,5 @@
 package com.onebox.demoOneBox.model;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,19 +7,9 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-@Table(name = "product")
 public class Product {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productid;
-
     private String description;
-
     private int amount;
-
-    @ManyToOne()
-    @JoinColumn(name = "cart_id")
-    private Cart cart;
+    private String cart;
 }
